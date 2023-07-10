bda8e907cbe87f327f0c7ab44be29ef2aba549fc
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-11 00:45:28 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,7 @@ echo "Cloning Repo...."
 git clone https://github.com/konichiwa55115/searchqurey /kony
 cd /kony
 pip3 install -r requirements.txt
+wget https://archive.org/compress/booksbot48525x
+unzip booksbot48525x
 echo "Starting Bot...."
 gunicorn app:app & python3 bot.py
