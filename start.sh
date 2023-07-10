dee9013b44a1222734759539a371f611c1c39e7e
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-11 00:35:40 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/konichiwa55115/audmerg /kony
+git clone https://github.com/konichiwa55115/searchqurey /kony
 cd /kony
 pip3 install -r requirements.txt
 echo "Starting Bot...."
