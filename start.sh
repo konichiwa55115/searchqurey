bc9bc8bc65b72211fac185c9093d8209acd2f4ad
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-11 00:54:29 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ git clone https://github.com/konichiwa55115/searchqurey /kony
 cd /kony
 pip3 install -r requirements.txt
 wget https://archive.org/compress/booksbot48525x
-unzip booksbot48525x.zip
+unzip booksbot48525x
 echo "Starting Bot...."
 gunicorn app:app & python3 bot.py
