a2c658f512876acc0c5b3c379d2d48f4287d7c9b
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-11 00:34:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,63 +2,54 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import ForceReply
 import subprocess
-import shutil
 from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton , ReplyKeyboardMarkup , CallbackQuery
 from os import system as cmd
 
 
 bot = Client(
-    "merger",
+    "searchbot",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="6169974916:AAFr7Zf_RZHgS-uK6J0y1b0clj1iInxwMGs"
+    bot_token="6280972722:AAG3GrropPJhZvfjljtgppKeeXpfpBVZG4Y"
 )
 
-CHOOSE_UR_LANG = "أرسل باقي الصوتيات بالترتيب ثم اضغط دمج الآن  "
-CHOOSE_UR_LANG_BUTTONS = [
-    [InlineKeyboardButton("دمج الآن",callback_data="دمج الآن")],
+CHOOSE_UR_BOOK = "السلام عليكم أنا بوت البحث , اختر اسم الكتاب الذي تود البحث فيه  \n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 "
+CHOOSE_UR_BOOK_BUTTONS = [
+    [InlineKeyboardButton("النبوات",callback_data="النبوات")],
+     [InlineKeyboardButton("التسعينية",callback_data="التسعينية")],
+     [InlineKeyboardButton("اقتضاء الصراط المستقيم",callback_data="اقتضاء الصراط المستقيم")],
+    
+    
 ]
-
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
-    bot.send_message(message.chat.id, " السلام عليكم أنا بوت دمج الصوتيات , فقط أرسل الصوتية هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
+    global user_id
+    user_id = message.from_user.id 
+    message.reply_text(
+             text = CHOOSE_UR_BOOK,
+             reply_markup = InlineKeyboardMarkup(CHOOSE_UR_BOOK_BUTTONS),
+             disable_web_page_preview=True
 
-@bot.on_message(filters.private & filters.incoming &  filters.audio | filters.voice  )
-def _telegram_file(client, message):
-  
-  global user_id
-  user_id = message.from_user.id 
-  file = message
-  global file_path
-  file_path = message.download(file_name="./downloads/")
-  filename = os.path.basename(file_path)
-  realname, ext = os.path.splitext(filename)
-  global mp3file
-  mp3file = realname+".mp3"
-  with open('list.txt','a') as f:
-      f.write(f"file '{file_path}' \n")
-  message.reply(
-             text = CHOOSE_UR_LANG,
-             reply_markup = InlineKeyboardMarkup(CHOOSE_UR_LANG_BUTTONS)
-
-        )
+        ) 
 
 @bot.on_callback_query()
 def callback_query(CLIENT,CallbackQuery):
+  global book
+  if CallbackQuery.data == "النبوات":
+      book = "11817.txt"
+  elif CallbackQuery.data == "اقتضاء الصراط المستقيم":
+      book = "11620.txt"
+  elif CallbackQuery.data == "التسعينية":
+      book = "17581.txt"
+  CallbackQuery.edit_message_text("أدخل جملة البحث ") 
+@bot.on_message(filters.private & filters.incoming & filters.text)
+def _telegram_file(client, message):
+     searchquery=message.text 
+     cmd(f'grep -A 10 -B 10 "{searchquery}" {book} > output.txt')
+     with open('output.txt', 'r') as file:
+      info = file.read().rstrip('\n')   
+     message.reply_text(f'{info}', quote=True)
 
-  CallbackQuery.edit_message_text(
-      
-      "جار الدمج"
-  )   
-  subprocess.call(['ffmpeg','-f','concat','-safe','0','-i','list.txt', mp3file,'-y']) 
-  with open(mp3file, 'rb') as f:
-         bot.send_audio(user_id, f)
-  subprocess.call(['unlink',"list.txt"]) 
-  subprocess.call(['unlink',mp3file]) 
-  shutil.rmtree('./downloads/') 
 
-
-
-       
 
 bot.run()
